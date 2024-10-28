5a7e3ee6c99aa0e3740088a354eb65c7e5fa0fdd
AlanManjarrez <160210523+AlanManjarrez@users.noreply.github.com>
2024-10-27 19:44:54 -0700

    Persistencia
    Creación de la persistencia

---
+++ pf_appweb_persistencia/src/main/java/pf_appweb_persistencia_entity/Post.java
@@ -0,0 +1,56 @@
+
+package pf_appweb_persistencia_entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Jose Alan Manjarrez Ontiveros 228982
+ * @author Jesus Eduardo Villanueva Godoy 235078
+ */
+@Entity
+public class Post implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Post)) {
+            return false;
+        }
+        Post other = (Post) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pf_appweb_persistencia_entity.Post[ id=" + id + " ]";
+    }
+    
+}
---
+++ pf_appweb_persistencia/src/main/java/pf_appweb_persistencia_main/Pf_appweb_persistencia.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pf_appweb_persistencia_main;
+
+/**
+ *
+ * @author Jose Alan Manjarrez Ontiveros 228982
+ * @author Jesus Eduardo Villanueva Godoy 235078
+ */
+public class Pf_appweb_persistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
